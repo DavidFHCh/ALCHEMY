47d5cc41d2c10dd3d7110adad905bcf3a165c1f9
DavidFHCh <davfep_96@ciencias.unam.mx>
2018-03-13 23:51:41 -0600

    renaming.

--- myserver/myserver/urls.py
+++ myserver/myserver/urls.py
@@ -1,4 +1,4 @@
-"""alchemy URL Configuration
+"""myserver URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
