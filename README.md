eee803152fd0aff45d09cd630a9482e71a5c5331
DavidFHCh <davfep_96@ciencias.unam.mx>
2018-03-14 00:21:14 -0600

    Adding .gitignore
    Creating our first views.
    
    This is kind of simple.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ALCHEMY
+
+version of python used: Python 3.6.4
+
+version of Django used: Django 2.0.3
--- myserver/alchemy/views.py
+++ myserver/alchemy/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("In the future this will be a beautiful site.")
 
 # Create your views here.
