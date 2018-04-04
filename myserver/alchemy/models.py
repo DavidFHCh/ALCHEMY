98ba9cc22006f2564c4b9317e84acbe7cdc81d1d
LesterTremens <orapro@me.com>
2018-04-03 20:44:17 -0500

    Base creada

--- myserver/alchemy/models.py
+++ myserver/alchemy/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+# Primer y creo unico modelo va a ser elementos incluye los atributos.
+
+class Elementos(models.Model):
+    Nombre_Elemento = models.CharField(max_length = 35)
+    Simbolo_Elemento = models.CharField(max_length = 35)
+    Informacion_Elemento = models.CharField(max_length = 200)
+    Foto_Elemento = models.CharField(max_length = 50)
+    Foto_Tarjeta = models.CharField(max_length = 50)
+
+    def Nombre(self):
+        cadena = "{0}"
+        return cadena.format(self.Nombre_Elemento)
+
+    def Simbolo(self):
+        cadena = "{0}"
+        return cadena.format(self.Simbolo_Elemento)
+
+    def Informacion(self):
+        cadena = "{0}"
+        return cadena.format(self.Informacion_Elemento)
+
+    def Ruta_Elemento(self):
+        cadena = "{0}"
+        return cadena.format(self.Foto_Elemento)
+
+    def Ruta_Tarjeta(self):
+        cadena = "{0}"
+        return cadena.format(self.Foto_Tarjeta)
